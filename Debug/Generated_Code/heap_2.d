f47f3f6658d3ee7d6d21081aeb440d3cc7bb0c1e
Manuel Wittmer <ma_wittmer@hotmail.com>
2016-10-11 16:09:00 +0200

    delet

--- Debug/Generated_Code/heap_2.d
+++
@@ -1,4 +0,0 @@
-Generated_Code/heap_2.o: ../Generated_Code/heap_2.c \
- ../Generated_Code/FreeRTOSConfig.h
-
-../Generated_Code/FreeRTOSConfig.h:
--- Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- /Users/manuelwittmer/workspace.kds/PAIND/Generated_Code/IO_Map.h \
- /Users/manuelwittmer/workspace.kds/PAIND/Static_Code/IO_Map/MKL25Z4.h
-
-/Users/manuelwittmer/workspace.kds/PAIND/Generated_Code/IO_Map.h:
-
-/Users/manuelwittmer/workspace.kds/PAIND/Static_Code/IO_Map/MKL25Z4.h:
